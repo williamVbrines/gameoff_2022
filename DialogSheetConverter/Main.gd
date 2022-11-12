3452171f5420c7eded7444a37ef07bd586d95958
WilliamVB <williamvb@protonmail.com>
2022-11-11 21:46:32 -0800

    Made Dialog sheet Converter

--- DialogSheetConverter/Main.gd
+++ DialogSheetConverter/Main.gd
@@ -1,30 +1,24 @@
-###############################################################################
-#William Brines , Fairy Bound LLC
-#Last updated 12-1-2020
-#Date creatated 12-1-2020
-#This class parses dialog from a json file and manages what each command tang
-# and other dialog comming in from that json file.
-###############################################################################
 extends Node2D
-##############################################################
 
-export var _file_path : String = "null";#The path of the dialog file
+@export var _file_path : String = "null";#The path of the dialog file
 
 var _raw_file_content : String = "null";
 
-var d = {"a" : "a", "b" : "b"}
+func _ready() -> void:
+	var res = load("res://out.tres") as DialogData;
+	print(res.data);
+	
 ################################################################################
 #This function when called saves a dictionary to a file.
 ################################################################################
 func save_json_file(path : String, content : Dictionary) -> void:
-	var file = File.new()
-	if (file.open(path, File.WRITE)) != 0:
+	var file = FileAccess.open(path , FileAccess.WRITE)
+
+	if file != null:
 		print("!!Error : could not open file " + path );
-		file.close();
 		breakpoint;
 	else:
-		file.store_line(to_json(content));
-		file.close();
+		file.store_line(JSON.new().stringify(content));
 	
 	
 ################################################################################
@@ -34,15 +28,14 @@ func save_json_file(path : String, content : Dictionary) -> void:
 #will be set.
 ################################################################################
 func load_file(path : String) -> String:
-	var file = File.new();
+	
+	var file = FileAccess.open(path, FileAccess.READ)
 	var data = "";
 	
-	if (file.open(path, File.READ)) != 0:
+	if file == null:
 		$Label.text = "!!Error : could not open file " + path ;
-		file.close();
 	else:
 		data = file.get_as_text();
-		file.close();
 		
 	return data;
 	
@@ -51,36 +44,16 @@ func _on_Button_pressed():
 	$Label.text = "Processing" + $LineEdit.text;
 	
 	var new_content = _convert_to_json(load_file($LineEdit.text));
-
+	print(new_content);
+	print(JSON.parse_string(new_content));
+	save_data(new_content)
 	
-#	print(new_content);
-#	var _file_content = parse_json(new_content);
-#	print(_file_content);
-#	if _file_content is Dictionary:
-#
-##C:\Users\William\Downloads\leek.trs
-#		var maxIndex = $LineEdit.text.length() -1;
-#		var path = $LineEdit.text;
-#
-#		print(_file_content);
-#		print(path)
-#
-#		for i in maxIndex:
-#			if path[maxIndex-i] == '.':
-#				path.erase(maxIndex-i,maxIndex);
-#				path += ".json";
-#				break
-#
-#		print(path)
-#
-#		save_json_file(path, _file_content);
-#
-#		$Label.text = "Processing of " + $LineEdit.text + " complete";
-#
-#		$LineEdit.text = "";
-#
-#
-#
+	
+func save_data(data : String) -> void:
+	var dialog_data = DialogData.new();
+	dialog_data.data = JSON.parse_string(data);
+	ResourceSaver.save(dialog_data,"res://out.tres")
+	
 func _convert_to_json(data : String) -> String:
 	var out = "{\n";
 
@@ -146,6 +119,8 @@ func _convert_to_json(data : String) -> String:
 					act += data.substr(index , data.find("\t", index) - index);
 					index = data.find("\t",index);
 					
+					act = act.replace("\"", "");
+					
 					var entry = "";
 					var i = 0;
 					while i < act.length():
@@ -180,14 +155,48 @@ func _convert_to_json(data : String) -> String:
 					tabNum = 4;
 					
 					index += 1;
-					def = "[";
-					def += data.substr(index , data.find("\n", index) - index);
-					def += "]";
+					def = "";
+					def += data.substr(index , data.find("\n", index) - index -1);
+					
+					def = def.replace("\"", "");
+					
+					var entry = "";
+					var i = 0;
+					
+					while i < def.length():
+						var add = "";
+						
+						if def[i] == ';':
+							
+							if i < def.length()-1:#If not the last char
+								if def[max(0,i-1)] == '\"':  add += ",";
+								else: add += "\",";
+									
+								if def[i+1] != '\"': add += "\"";
+							else:
+								if def[max(0,i-1)] != '\"': add += "\"";
+								
+							entry += add;
+						
+							
+						else:
+							entry += def[i]
+							
+						i += 1;
+					
+					
+					if entry.length() >= 1:
+						if def[0] != '\"':
+							entry = "\"" + entry;
+						
+						
+					def = entry;
+					
+					out += "\t\t\"def\": [" + def + "]\n";
 					
 					index = data.find("\n",index);
 					index = data.length() if index == -1 else index;
 					
-					out += "\t\t\"def\": " + def + "\n";
 					
 				_:
 					index += 1;
@@ -198,16 +207,19 @@ func _convert_to_json(data : String) -> String:
 		else:
 			out += data[index];
 			index += 1;
+			
 	out += "\t},\n";
-	out += "\t\"order\":[" ;
-	for i in order.size()-1:
-		out += "\"" + str(order[i]) + "\",";
-	out += "\"" + str(order[order.size()-1]) + "\"";
-	out += "]\n";
+	if order.size() > 0:
+		out += "\t\"order\":[" ;
+		for i in order.size()-1:
+			out += "\"" + str(order[i]) + "\",";
+		out += "\"" + str(order[order.size() -1]) + "\"";
+		out += "]\n";
+
 	out += "\n}"
-	
-	print(out)
+
 	return out;
+	
 	
 func _on_PopUP_pressed():
 	if $FileDialog.visible == true:
