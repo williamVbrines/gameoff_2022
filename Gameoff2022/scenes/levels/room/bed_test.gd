52155a1c12251cc891a6f35c6821e45609701e6f
WilliamVB <williamvb@protonmail.com>
2022-11-23 01:34:25 -0800

    Fixed connection bug in RoomIteractables

--- Gameoff2022/scenes/levels/room/bed_test.gd
+++ Gameoff2022/scenes/levels/room/bed_test.gd
@@ -2,6 +2,7 @@ extends Node3D
 class_name RoomInteractable3D
 
 @export var mesh : MeshInstance3D;
+@export var area : Area3D;
 
 @export var message : String = "Keep Calm this is just a test"
 @export var tooltip : String = "test of tool tip"
@@ -9,18 +10,26 @@ class_name RoomInteractable3D
 
 var mouse_in_area : bool = false;
 
+func _ready() -> void:
+	_make_connections();
+	
+func _make_connections()->void:
+	area.mouse_entered.connect(_on_mouse_entered);
+	area.mouse_exited.connect(_on_mouse_exited);
+	
+	
 func _unhandled_input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("mouse_left") && mouse_in_area:
 		EventManager.room_show_confirm.emit(message,action);
 		EventManager.room_hide_tooltip.emit();
 
-func _on_area_3d_mouse_entered() -> void:
+func _on_mouse_entered() -> void:
 	EventManager.room_show_tooltip.emit(tooltip);
 	_hovered();
 
 
-func _on_area_3d_mouse_exited() -> void:
+func _on_mouse_exited() -> void:
 	EventManager.room_hide_tooltip.emit();
 	_unhovered();
 
