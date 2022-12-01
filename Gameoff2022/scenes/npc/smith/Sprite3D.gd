bb0f84f9e16309e1bf52e5462ffbb8b2aa1e3c2b
WilliamVB <williamvb@protonmail.com>
2022-12-01 00:16:43 -0800

    added inkeep

--- Gameoff2022/scenes/npc/smith/Sprite3D.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite3D
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	animation_player.play("default")
